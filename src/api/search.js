6074f0cf62c4ba0e3d1de8b0bd53a1d2f730bb4d
nigorora14 <niltongr@outlook.com>
2021-07-07 11:46:25 -0500

    .

--- src/api/search.js
+++ src/api/search.js
@@ -1,26 +1,39 @@
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import {SEARCH_HISTORY} from '../utils/constants'
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { size } from "lodash";
+import { API_URL, SEARCH_HISTORY } from "../utils/constants";
+import { sortArrayByDate } from "../utils/functions";
 
-export async function getSearchHistoryApi(){
-    try {
-        const history = await AsyncStorage.getItem(SEARCH_HISTORY)
-        if (!history) {
-            return []
-        }
-        return JSON.parse(history)
-    } catch (error) {
-        console.log(error)
-        return []
-    }
-    
+export async function getSearchHistory() {
+  try {
+    const history = await AsyncStorage.getItem(SEARCH_HISTORY);
+    if (!history) return [];
+
+    return sortArrayByDate(JSON.parse(history));
+  } catch (e) {
+    return [];
+  }
 }
 
-export async function updateSearchHistoryApi(search){
-    const history = await getSearchHistoryApi()
+export async function updateSearchHistory(search) {
+  const history = await getSearchHistory();
 
-    history.push({
-        search,
-        date: new Date()
-    })
-    await AsyncStorage.setItem(SEARCH_HISTORY, JSON.stringify(history))
+  if (size(history) > 5) history.pop();
+
+  history.push({
+    search,
+    date: new Date(),
+  });
+  await AsyncStorage.setItem(SEARCH_HISTORY, JSON.stringify(history));
+}
+
+export async function searchProductsApi(search){
+    try {
+        const url = `${API_URL}/products?_q=${search}&_limit=40`
+        const response= await fetch(url)
+        const result= await response.json()
+        return result
+    } catch (error) {
+        console.log(error)
+        return null
+    }
 }
